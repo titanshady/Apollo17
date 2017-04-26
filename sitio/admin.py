f95ea8e4d74c00f50fe9a9afe46a92778cd4dbd3
titanshady <ricardo.pinheiro15@hotmail.com>
2017-04-26 20:19:51 -0300

    Att Model Astronautas // Add Model Indicadores

--- sitio/admin.py
+++ sitio/admin.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 
-from .models import initmes, mission, valore, apollo, gemini, mercury, portfc, portfl
-
+from .models import initmes, mission, valore, portfc, portfl, indicador,astronauta, portfcl
 admin.site.register(initmes)
 admin.site.register(mission)
 admin.site.register(valore)
-admin.site.register(apollo)
-admin.site.register(gemini)
-admin.site.register(mercury)
 admin.site.register(portfc)
 admin.site.register(portfl)
+admin.site.register(indicador)
+admin.site.register(astronauta)
+admin.site.register(portfcl)
--- sitio/models.py
+++ sitio/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
 class initmes(models.Model):
 	descript = models.TextField('Mensagem de Início')
@@ -8,8 +9,8 @@ class initmes(models.Model):
 
 
 	class Meta:
-		verbose_name = 'Mensagem Inicial'
-		verbose_name_plural = 'Mensagens Iniciais'
+		verbose_name = '1 - Mensagem Inicial'
+		verbose_name_plural = '1 - Mensagens Iniciais'
 
 class mission(models.Model):
 	title = models.TextField('Mensagem Principal',)
@@ -20,8 +21,8 @@ class mission(models.Model):
 
 
 	class Meta:
-		verbose_name = 'Texto: Andamento da Missão'
-		verbose_name_plural = 'Texto: Andamento da Missão'		
+		verbose_name = '3 - Andamento da Missão: Texto'
+		verbose_name_plural = '3 - Andamento da Missão: Texto'		
 
 class valore(models.Model):
 	item = models.TextField('Missões',)
@@ -32,12 +33,19 @@ class valore(models.Model):
 
 
 	class Meta:
-		verbose_name = 'Conquista da Empresa'
-		verbose_name_plural = 'Conquistas da Empresa'
+		verbose_name = '2 - Conquista da Empresa'
+		verbose_name_plural = '2 - Conquistas da Empresa'
 
-class apollo(models.Model):
+class astronauta(models.Model):
+	APPROVAL_CHOICES = (
+    ('apollo', 'Apollo'),
+    ('gemini', 'Gemini'),
+    ('mercury', 'Mercury'),
+	)
+
 	name = models.CharField('Nome', max_length=100,)
 	text = models.TextField('Texto',)
+	tipo = models.CharField(max_length=9, choices=APPROVAL_CHOICES, default='apollo')
 	photo = models.ImageField(upload_to='sitio/images/astronautas', verbose_name='Foto',)
 
 	def __str__(self):
@@ -45,34 +53,8 @@ class apollo(models.Model):
 
 
 	class Meta:
-		verbose_name = 'Astronauta Apollo'
-		verbose_name_plural = 'Astronautas Apollo'
-
-class gemini(models.Model):
-	name = models.CharField('Nome', max_length=100,)
-	text = models.TextField('Texto',)
-	photo = models.ImageField(upload_to='sitio/images/astronautas', verbose_name='Foto',)
-
-	def __str__(self):
-		return self.name
-
-
-	class Meta:
-		verbose_name = 'Astronauta Gemini'
-		verbose_name_plural = 'Astronautas Gemini'
-
-class mercury(models.Model):
-	name = models.CharField('Nome', max_length=100,)
-	text = models.TextField('Texto',)
-	photo = models.ImageField(upload_to='sitio/images/astronautas', verbose_name='Foto',)
-
-	def __str__(self):
-		return self.name
-
-
-	class Meta:
-		verbose_name = 'Astronauta Mercury'
-		verbose_name_plural = 'Astronautas Mercury'
+		verbose_name = '5 - Astronauta'
+		verbose_name_plural = '5 - Astronautas'
 
 class portfc(models.Model):
 	descript = models.TextField('Nome & Descrição da Empresa',)
@@ -83,8 +65,8 @@ class portfc(models.Model):
 
 
 	class Meta:
-		verbose_name = 'Portifólio - Em Construção'
-		verbose_name_plural = 'Portifólio - Em Construção'
+		verbose_name = '6 - Portifólio - Em Construção'
+		verbose_name_plural = '6 - Portifólio - Em Construção'
 
 class portfl(models.Model):
 	descript = models.TextField('Nome & Descrição da Empresa',)
@@ -95,5 +77,46 @@ class portfl(models.Model):
 
 
 	class Meta:
-		verbose_name = 'Portifólio - Lançado'
-		verbose_name_plural = 'Portifólio - Lançados'
+		verbose_name = '7 - Portifólio - Lançado'
+		verbose_name_plural = '7 - Portifólio - Lançados'
+
+class indicador(models.Model):
+
+	APPROVAL_CHOICES = (
+    ('1', 'Azul'),
+    ('2', 'Laranja'),
+    ('3', 'Amarelo'),
+    ('4', 'Marrom'),
+	)
+
+	name = models.CharField('Nome', max_length=100,)
+	percent = models.PositiveIntegerField(primary_key=True, validators=[MaxValueValidator(100)])
+	color = models.CharField(max_length=1, choices=APPROVAL_CHOICES, default='1')
+
+	def __str__(self):
+		return self.name
+
+
+	class Meta:
+		verbose_name = '4 - Andamento da Missão: Indicador'
+		verbose_name_plural = '4 - Andamento da Missão: Indicadores'
+
+
+class portfcl(models.Model):
+
+	APPROVAL_CHOICES = (
+    ('const', 'Em Construção'),
+    ('lanc', 'Lançados'),
+	)
+
+	descript = models.TextField('Nome & Descrição da Empresa',)
+	logo = models.ImageField(upload_to='sitio/images/portifolio', verbose_name='Logo',)
+	andament = models.CharField(max_length=9, choices=APPROVAL_CHOICES, default='const')
+
+	def __str__(self):
+		return self.descript
+
+
+	class Meta:
+		verbose_name = '8 - Portifólio (Em Desenvolvimento)'
+		verbose_name_plural = '8 - Portifólio (Em Desenvolvimento)'
